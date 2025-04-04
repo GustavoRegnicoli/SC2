68f731ecdef020aecc7f02bf7ba2afbe6e63cd2e
GustavoRegnicoli <gustavo.regnicoli@mi.unc.edu.ar>
2025-04-04 16:41:22 -0300

    Actualización automática

--- item1.m
+++ item1.m
@@ -11,12 +11,23 @@ Mat_A=[-R/L, -1/L; 1/C 0];
 Mat_B=[1/L; 0];
 % Vector de salida
 Mat_C=[0 R];
+
+
 % La inversa de la matriz de los valores propios
 ieig=-1*(1./(eig(Mat_A)));
 tR=min(ieig) %Constante de tiempo mas rapida
 tL=max(ieig) %Constante de tiempo mas lerda
+
+% [numerador, denominador] = ss2tf(Mat_A, Mat_B, Mat_C, 0);
+% G = tf(numerador, denominador);
+% figure
+% step(G*12)
+
+
+
+
 % Se define el paso h, el tiempo fina Tf y la cantidad de pasos
-h=tR;Tf=0.04;pasos=Tf/h;
+h=real(tR);Tf=0.04;pasos=Tf/h;
 % Parametros para la fuente
 t=[];
 talt=10e-3;
@@ -24,18 +35,19 @@ ii=1;
 % Condiciones iniciales
 i(1)=0;Vc(1)=0;Ve=12;
 X0=[i(1);Vc(1)];x=[0;0];
-
+vo(1)=0;
 while(ii<(pasos+1))
     t(ii)=ii*h;
     u(ii)=Ve;
-    if(t(ii)>talt)% Cada 10ms la tension se invierte
-        Ve=Ve*-1;
-        talt=talt+10e-3;
-    end
+    % if(t(ii)>talt)% Cada 10ms la tension se invierte
+    %     Ve=Ve*-1;
+    %     talt=talt+10e-3;
+    % end
     i(ii)=x(1);Vc(ii)=x(2);
     xp=Mat_A*(x-X0)+Mat_B*u(ii);
     x=x+h*xp;
     y=Mat_C*x;
+    vo(ii)=y;
     ii=ii+1;
 end
 % Se grafica la corriente i, la tension del capacitor Vc y la tension de
@@ -44,3 +56,6 @@ figure(1);hold on;
 subplot(3,1,1);plot(t,i);grid on; title('i corriente');
 subplot(3,1,2);plot(t,Vc);grid on; title('Vc tension del capacitor');
 subplot(3,1,3);plot(t,u);grid on; title('Ve entrada');
+figure
+plot(t,vo);grid on; title('Vo salida');
+
